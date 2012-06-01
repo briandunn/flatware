5d71c6ca10be053f96702ed315063149b36434df
Brian Dunn and Joshua Davey <dev+briandunn+jgdavey@hashrocket.com>
2012-06-01 16:37:09 -0500

    Merge cucumber reboot preventer from hr branch

--- lib/flatware/cucumber.rb
+++ lib/flatware/cucumber.rb
@@ -7,7 +7,29 @@ module Flatware
     end
 
     def run(options, out, error)
-      ::Cucumber::Cli::Main.new(Array(options), out, error).execute!
+      cli = ::Cucumber::Cli::Main.new(Array(options), out, error)
+      runtime.configure(cli.configuration)
+
+      runtime.instance_eval do
+        remove_instance_variable(:@loader) if @loader
+        tree_walker = @configuration.build_tree_walker(self)
+        self.visitor = tree_walker
+        tree_walker.visit_features(features)
+      end
+    end
+
+    def runtime
+      @runtime ||= preload
+    end
+
+    private
+    def preload
+      require 'cucumber' unless defined?(::Cucumber::Cli)
+      configuration = ::Cucumber::Cli::Configuration.new
+      configuration.parse! []
+      runtime = ::Cucumber::Runtime.new(configuration)
+      runtime.send :load_step_definitions
+      runtime
     end
   end
 end
