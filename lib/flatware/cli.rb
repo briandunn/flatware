6ebec2860ed7ffdd8f6a542b45882c6986768811
Brian Dunn, Ingemar Edsborn, and Jon Allured <dev+briandunn+ingemar+jonallured@hashrocket.com>
2012-06-12 13:30:03 -0500

    add mux command: start workers in tmux panes

--- lib/flatware/cli.rb
+++ lib/flatware/cli.rb
@@ -1,6 +1,18 @@
 require 'thor'
 module Flatware
   class CLI < Thor
+    desc "mux", "splits your current tmux pane into worker panes. Runs workers in them."
+    def mux
+      processors.times do |env_number|
+        spawn({'TEST_ENV_NUMBER' => env_number.to_s}, <<-SHELL
+            tmux send-keys -t `tmux split-window -h -P` "flatware work; exit" C-m
+          SHELL
+        )
+      end
+      system "tmux select-layout even-horizontal"
+      dispatch
+    end
+
     desc "dispatch", "fire up the dispatcher to distribute tests"
     def dispatch
       Dispatcher.dispatch!
@@ -9,6 +21,12 @@ module Flatware
     desc "work", "request and perform work from a dispatcher"
     def work
      Worker.listen!
+    end
+
+    private
+
+    def processors
+      @processors ||= `hostinfo`.match(/^(?<processors>\d+) processors are logically available\.$/)[:processors].to_i
     end
   end
 end
