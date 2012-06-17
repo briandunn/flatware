d2adbf2ca61901b1cad05c00e7f15d71a6554d5a
Brian Dunn <dev+briandunn@hashrocket.com>
2012-06-17 18:09:12 -0500

    Encapsulate controlled message processor

--- lib/flatware/worker.rb
+++ lib/flatware/worker.rb
@@ -6,11 +6,8 @@ module Flatware
         @context ||= ZMQ::Context.new
       end
 
-      def die
-        @die ||= context.socket(ZMQ::SUB).tap do |die|
-          die.connect 'ipc://die'
-          die.setsockopt ZMQ::SUBSCRIBE, ''
-        end
+      def fireable
+        @fireable ||= Fireable.new(context)
       end
 
       def task
@@ -19,24 +16,45 @@ module Flatware
         end
       end
 
-      def listen_to_boss
-        die
-      end
-
       def clock_in
         task.send 'hi'
       end
 
       def close_up
-        [die, task, context].each &:close
+        [task, context].each &:close
       end
 
       def listen!
         clock_in
-        listen_to_boss
-        die.recv
+        fireable.until_fired task do |message|
+          Cucumber.run message, $stdout, $stderr
+          task.send 'done'
+        end
+        puts Cucumber::Formatter.all_summaries
         close_up
       end
+    end
+  end
+
+  class Fireable
+    def initialize(context)
+      @die = context.socket(ZMQ::SUB).tap do |die|
+        die.connect 'ipc://die'
+        die.setsockopt ZMQ::SUBSCRIBE, ''
+      end
+    end
+
+    attr_reader :die
+
+    def until_fired(sockets=[], &block)
+      quit = false
+      while !quit && (ready = ZMQ.select(Array(sockets) + [die]))
+        messages = ready.flatten.compact.map(&:recv)
+        return if messages.include? 'seppuku'
+        messages.each &block
+      end
+    ensure
+      die.close
     end
   end
 end
