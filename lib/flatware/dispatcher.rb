c221f2fe1ab250545cc8fbc649ea7e9b3deca398
Brian Dunn and Joshua Davey <dev+briandunn+jgdavey@hashrocket.com>
2012-07-02 17:30:26 -0500

    Dispatcher always be dispatchin'

--- lib/flatware/dispatcher.rb
+++ lib/flatware/dispatcher.rb
@@ -11,19 +11,10 @@ module Flatware
       def dispatch!
         features = Cucumber.features
 
-        dispatched = 0
-
         fireable.until_fired dispatch do |request|
-          if request != 'hi'
-            # request is a test result
-            dispatched -= 1
-          end
           feature = features.pop
           if feature
             dispatch.send feature
-            dispatched += 1
-            log "Not yet dispatched: #{features.length}"
-            log "       In progress: #{dispatched}"
           else
             dispatch.send 'seppuku'
           end
