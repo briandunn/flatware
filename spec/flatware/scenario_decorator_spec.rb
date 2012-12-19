9dd9914e6e6a47c50090777a0a1fb7095b7b1d8c
Brian Dunn <dev+briandunn@hashrocket.com>
2012-12-19 16:42:48 -0600

    Improve failure messages

--- spec/flatware/scenario_decorator_spec.rb
+++ spec/flatware/scenario_decorator_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe Flatware::ScenarioDecorator do
-  let(:scenario) { stub file_colon_line: "features/file_line.feature:3", name: "scenario_name", status: :passed }
-  let(:scenario_outline) { stub file_colon_line: "features/outline.feature:3", name: "outline_name" }
-  let(:example_row) { stub scenario_outline: scenario_outline, status: :passed }
+  let(:scenario) { double 'Scenario', file_colon_line: "features/file_line.feature:3", name: "scenario_name", status: :passed }
+  let(:scenario_outline) { double 'Scenario Outline', file_colon_line: "features/outline.feature:3", name: "outline_name" }
+  let(:example_row) { double 'ExampleRow', scenario_outline: scenario_outline, status: :passed }
 
   context 'with scenario' do
     subject { Flatware::ScenarioDecorator.new(scenario) }
-    it { subject.file_colon_line.should eq scenario.file_colon_line }
-    it { subject.name.should eq scenario.name }
-    it { subject.status.should eq scenario.status }
+    its(:file_colon_line) { should eq scenario.file_colon_line }
+    its(:name) { should eq scenario.name }
+    its(:status) { should eq scenario.status }
   end
 
   context 'with example row' do
     subject { Flatware::ScenarioDecorator.new(example_row) }
-    it { subject.file_colon_line.should eq example_row.scenario_outline.file_colon_line }
-    it { subject.name.should eq example_row.scenario_outline.name }
-    it { subject.status.should eq example_row.status }
+    its(:file_colon_line) { should eq example_row.scenario_outline.file_colon_line }
+    its(:name) { should eq example_row.scenario_outline.name }
+    its(:status) { should eq example_row.status }
   end
 
 end
