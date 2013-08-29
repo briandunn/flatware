e194defd79f4a527605c8dd36ea87730602cd8cd
Christopher Erin <chris.erin@gmail.com>
2013-08-29 15:44:18 -0500

    pids on linux system (no -c option)

--- lib/flatware/pids.rb
+++ lib/flatware/pids.rb
@@ -2,8 +2,17 @@ module Flatware
   extend self
   # All the pids of all the processes called flatware on this machine
   def pids
-    `ps -c -opid,command`.split("\n").map do |row|
+    pids_command.split("\n").map do |row|
       row =~ /(\d+).*flatware/ and $1.to_i
     end.compact
+  end
+
+  def pids_command
+    case ProcessorInfo.operating_system
+    when 'Darwin'
+      `ps -c -opid,command`
+    when 'Linux'
+      `ps -opid,command`
+    end
   end
 end
