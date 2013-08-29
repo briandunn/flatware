234709ab21d9a47501562b7c0bb1f269a83a78f7
Brian Dunn <dev+briandunn@hashrocket.com>
2013-08-29 15:42:06 -0500

    Fork the test worker too

--- spec/flatware/dispatcher_spec.rb
+++ spec/flatware/dispatcher_spec.rb
@@ -2,20 +2,26 @@ require 'spec_helper'
 
 describe Flatware::Dispatcher do
   context 'when a dispatcher is started' do
-    after { Flatware.close }
     it 'exits when fired' do
+      fork do
+        Flatware::Fireable.bind
+        socket = Flatware.socket ZMQ::REQ, connect: described_class::PORT
+        socket.send 'ready'
+        socket.recv
+        Flatware::Fireable.kill
+        Flatware.close
+      end
       pid = fork { described_class.start [:job] }
-      Flatware::Fireable.bind
-      wait_until { child_pids.include? pid }
-      Flatware::Fireable.kill
-      Process.wait pid
+      Process.waitall
       exit_statuses = Process.waitall.map(&:last)
       exit_statuses.all?(&:success?).should be
       child_pids.should_not include pid
     end
 
     it 'dispatches jobs' do
-      pid = fork { described_class.start [:job1, :job2] }
+      pid = fork do
+        described_class.start [:job1, :job2]
+      end
       socket = Flatware.socket ZMQ::REQ, connect: described_class::PORT
       socket.send 'ready'
       socket.recv.should eq :job1
@@ -23,8 +29,9 @@ describe Flatware::Dispatcher do
       socket.recv.should eq :job2
       socket.send 'ready'
       socket.recv.should eq 'seppuku'
+      Flatware.close
       Process.kill 'INT', pid
-      Process.wait pid
+      Process.waitall
     end
   end
 end
