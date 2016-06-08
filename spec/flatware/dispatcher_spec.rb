43d9770493585d5d6fa8961ce253ba090b680e32
Chris Erin and Dorian Karter <dev+chriserin+dkarter@hashrocket.com>
2016-06-08 15:37:39 -0500

    Temporarily fix race condition for dispatcher test

--- spec/flatware/dispatcher_spec.rb
+++ spec/flatware/dispatcher_spec.rb
@@ -4,6 +4,7 @@ describe Flatware::Dispatcher do
   context 'when a dispatcher is started' do
     let(:endpoint) {'ipc://test-dispatch'}
     it 'exits when fired' do
+
       fork do
         Flatware::Fireable.bind
         socket = Flatware.socket ZMQ::REQ, connect: endpoint
@@ -12,7 +13,10 @@ describe Flatware::Dispatcher do
         Flatware::Fireable.kill
         Flatware.close
       end
+
+      sleep 0.1
       pid = described_class.spawn [:job], endpoint
+
       exit_statuses = waitall.map(&:last)
       exit_statuses.all?(&:success?).should be
       child_pids.should_not include pid
@@ -27,6 +31,7 @@ describe Flatware::Dispatcher do
       socket.recv.should eq :job2
       socket.send 'ready'
       socket.recv.should eq 'seppuku'
+
       Flatware.close
       Process.kill 'INT', pid
       waitall
