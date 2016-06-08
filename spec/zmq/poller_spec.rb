6a9760d16b4b11bc8c0e6ccde6830feef40bedc5
Chris Erin and Dorian Karter <dev+chriserin+dkarter@hashrocket.com>
2016-06-08 14:06:24 -0500

    Setup monitor for socket before binding

--- spec/zmq/poller_spec.rb
+++ spec/zmq/poller_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe 'Our bad situation' do
   it 'Pub sub takes too long to init' do
-    pub_socket = Flatware.socket(ZMQ::PUB, bind: 'ipc://die')
+    pub_socket = Flatware.socket(ZMQ::PUB)
+    monitor = pub_socket.monitor
+    pub_socket.bind 'ipc://die'
 
     sub_socket = Flatware.socket(ZMQ::SUB, connect: 'ipc://die').tap do |die|
       die.setsockopt ZMQ::SUBSCRIBE, ''
     end
 
-    monitor = pub_socket.monitor
     true until monitor.recv == :EVENT_ACCEPTED
     pub_socket.send('something')
     message = sub_socket.recv
