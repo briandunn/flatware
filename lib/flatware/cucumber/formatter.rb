0ec33a5d1718d851c768daa5519a073284fd13e9
Brian Dunn and Vidal Ekechukwu <dev+briandunn+vekh@hashrocket.com>
2017-07-14 13:37:30 -0500

    Group steps by scenario

--- lib/flatware/cucumber/formatter.rb
+++ lib/flatware/cucumber/formatter.rb
@@ -4,19 +4,33 @@ require 'ostruct'
 module Flatware
   module Cucumber
     class Formatter
-      Checkpoint = Struct.new :steps, :scenarios do
+      Checkpoint = Struct.new :scenarios do
+        def steps
+          scenarios.flat_map(&:steps)
+        end
+
         def failures?
           scenarios.any? &:failed?
         end
       end
 
-      Scenario = Struct.new :name, :file_colon_line, :status do
+      Scenario = Struct.new :name, :file_colon_line do
+        attr_accessor :status
+
+        def steps
+          @steps ||= []
+        end
+
         def failed?
           status == :failed
         end
 
         def failed_outside_step?
           false
+        end
+
+        def self.from_test_case(test_case)
+          new test_case.name, test_case.location.to_s
         end
       end
 
@@ -34,12 +48,16 @@ module Flatware
         @steps = []
         @scenarios = []
         @matched_steps = Set.new
+        @current_scenario = nil
       end
 
-      attr_reader :steps, :scenarios, :matched_steps
+      attr_reader :steps, :scenarios, :matched_steps, :current_scenario
 
       def on_after_test_case(event)
-        scenarios << Scenario.new(event.test_case.name, event.test_case.location.to_s, event.result.to_sym)
+        scenario = current_scenario || Scenario.from_test_case(event.test_case)
+        scenario.status = event.result.to_sym
+        scenarios << current_scenario
+        @current_scenario = nil
       end
 
       def on_step_match(event)
@@ -48,13 +66,15 @@ module Flatware
 
       def on_after_test_step(event)
         if really_a_step?(event.test_step) or event.result.undefined?
-          steps << StepResult.new(event.result.to_sym, event.result.failed? && event.result.exception)
-          Sink::client.progress Result.new event.result.to_sym
+          @current_scenario ||= Scenario.from_test_case event.test_case
+          result = event.result
+          current_scenario.steps << StepResult.new(result.to_sym, result.failed? && result.exception)
+          Sink::client.progress Result.new result.to_sym
         end
       end
 
       def on_finished_testing(*)
-        Sink::client.checkpoint Checkpoint.new steps, scenarios
+        Sink::client.checkpoint Checkpoint.new scenarios
         reset
       end
 
