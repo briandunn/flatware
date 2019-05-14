8cfc0bf50b8979916510d26ac1f95bfead260bcc
Brian Dunn <brian.dunn@vineti.com>
2019-05-14 17:41:34 -0500

    Monkey patch status persist logic

--- lib/flatware/rspec/formatter.rb
+++ lib/flatware/rspec/formatter.rb
@@ -1,10 +1,30 @@
+# frozen_string_literal: true
+
+require 'rspec/core'
 require 'flatware/rspec/checkpoint'
 require 'flatware/rspec/summary'
+require 'flatware/sink'
 require 'rspec/core/formatters/console_codes'
+require 'rspec/core/example_status_persister'
 
 module Flatware
   module RSpec
     ProgressMessage = Struct.new(:progress)
+
+    # override this check.
+    # RSpec version assumes runner has loaded all spec files.
+    # But our runner has only loaded some.
+    ::RSpec::Core::ExampleStatusMerger.prepend(Module.new do
+      def example_must_no_longer_exist?(ex_id)
+        # Obviously, it exists if it was loaded for this spec run...
+        return false if @this_run.key?(ex_id)
+
+        spec_file = spec_file_from(ex_id)
+
+        # The example may still exist as long as the file exists...
+        !@file_exists_cache[spec_file]
+      end
+    end)
 
     class Formatter
       attr_reader :summary, :output
@@ -13,15 +33,15 @@ module Flatware
         @output = stdout
       end
 
-      def example_passed(example)
+      def example_passed(_example)
         send_progress :passed
       end
 
-      def example_failed(example)
+      def example_failed(_example)
         send_progress :failed
       end
 
-      def example_pending(example)
+      def example_pending(_example)
         send_progress :pending
       end
 
@@ -34,14 +54,14 @@ module Flatware
       end
 
       def close(*)
-        Sink::client.checkpoint Checkpoint.new(summary, @failure_notification)
+        Sink.client.checkpoint Checkpoint.new(summary, @failure_notification)
         @failure_notification = nil
       end
 
       private
 
       def send_progress(status)
-        Sink::client.progress ProgressMessage.new status
+        Sink.client.progress ProgressMessage.new status
       end
     end
 
