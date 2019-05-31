3cb2968f0b2dace2588cfa1e11161188d3357b23
Brian Dunn <brianpatrickdunn@gmail.com>
2019-05-31 15:42:03 -0500

    fix travis aruba announcing

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,20 +1,24 @@
+# frozen_string_literal: true
+
 require 'pathname'
 
-$:.unshift Pathname.new(__FILE__).dirname.join('../../lib').to_s
+$LOAD_PATH.unshift Pathname.new(__FILE__).dirname.join('../../lib').to_s
 
-ENV['PATH'] = [Pathname.new('.').expand_path.join('bin').to_s, ENV['PATH']].join(':')
+ENV['PATH'] = [Pathname('.').expand_path.join('bin'), ENV['PATH']].join(':')
 
 require 'flatware/pids'
 
 Before { @dirs = ['tmp', "aruba#{ENV['TEST_ENV_NUMBER']}"] }
 
 require 'aruba/cucumber'
+require 'aruba/api'
 require 'rspec/expectations'
 require 'flatware/processor_info'
 
 World(Module.new do
   def max_workers
     return 3 if travis?
+
     Flatware::ProcessorInfo.count
   end
 
@@ -25,34 +29,39 @@ end)
 
 Before do
   if travis?
-    @announce_stdout = true
-    @announce_stderr = true
-    @announce_cmd = true
-    @announce_dir = true
-    @announce_env = true
+    %i[
+      command
+      directory
+      environment
+      stderr
+      stdout
+    ].each(&aruba.announcer.method(:activate))
   end
 end
 
-After do |scenario|
-  if all_commands.count > 0
+After do |_scenario|
+  if all_commands.any?
     zombie_pids = Flatware.pids_of_group(all_commands[0].pid)
 
-    (Flatware.pids - [$$]).each do |pid|
+    (Flatware.pids - [$PROCESS_ID]).each do |pid|
       Process.kill 6, pid
     end
     Process.waitall
-    expect(zombie_pids.size).to(eq(0), "Zombie pids: #{zombie_pids.size}, should be 0")
+    expect(zombie_pids.size).to(
+      eq(0),
+      "Zombie pids: #{zombie_pids.size}, should be 0"
+    )
   end
 end
 
 After 'not @non-zero' do |scenario|
-  if flatware_process and scenario.status == :passed
+  if flatware_process && (scenario.status == :passed)
     expect(flatware_process.exit_status).to eq 0
   end
 end
 
 After '@non-zero' do |scenario|
-  if flatware_process and scenario.status == :passed
+  if flatware_process && (scenario.status == :passed)
     expect(flatware_process.exit_status).to eq 1
   end
 end
