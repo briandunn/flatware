8af189bed22e9225a39916ff21f54f6b4c979931
Brian Dunn <brianpatrickdunn@gmail.com>
2019-06-28 15:21:48 -0500

    no need to wait so long

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -1,5 +1,5 @@
 Aruba.configure do |config|
   config.working_directory = "tmp/aruba#{ENV['TEST_ENV_NUMBER']}"
-  config.exit_timeout = 3
-  config.io_wait_timeout = config.startup_wait_time = config.exit_timeout
+  config.startup_wait_time = 0.1
+  config.io_wait_timeout = config.exit_timeout = 3
 end
