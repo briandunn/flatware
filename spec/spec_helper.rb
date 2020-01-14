e01a3d0ba6ce7d74d9c40a4ffeac8a9951ae0fc5
Brian Dunn <brianpatrickdunn@gmail.com>
2020-01-14 13:14:09 -0600

    looks like Pathname glob is new

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,9 @@ require 'flatware/cucumber'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Pathname(__FILE__).dirname.glob('support/**/*.rb').sort.each { |f| require f }
+Dir[Pathname(__FILE__).dirname.join('support/**/*.rb')]
+  .sort
+  .each { |f| require f }
 
 RSpec.configure do |config|
   config.include WaitingSupport
