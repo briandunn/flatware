8c390a9e24f7a75c113c91c4c3ba743b8a6b9bb8
Brian Dunn <dev+briandunn@hashrocket.com>
2024-08-28 14:23:56 -0400

    upgrade cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,27 +14,9 @@ require 'rspec/expectations'
 
 World(Module.new do
   def max_workers
-    return 3 if travis?
-
     Etc.nprocessors
   end
-
-  def travis?
-    ENV.key? 'TRAVIS'
-  end
 end)
-
-Before do
-  if travis?
-    %i[
-      command
-      directory
-      environment
-      stderr
-      stdout
-    ].each(&aruba.announcer.method(:activate))
-  end
-end
 
 After do |_scenario|
   all_commands.reject(&:stopped?).each do |command|
@@ -54,10 +36,19 @@ After do |_scenario|
   end
 end
 
-After 'not @non-zero' do |scenario|
-  expect(flatware_process.exit_status).to eq 0 if flatware_process && (scenario.status == :passed)
+expect_flatware_exit = lambda do |expected_status|
+  lambda do |scenario|
+    return unless scenario.status == :passed && flatware_process
+
+    status = begin
+      last_command_stopped.exit_status
+    rescue Aruba::NoCommandHasBeenStoppedError
+      flatware_process.wait.exitstatus
+    end
+
+    expect(status).to eq expected_status
+  end
 end
 
-After '@non-zero' do |scenario|
-  expect(flatware_process.exit_status).to eq 1 if flatware_process && (scenario.status == :passed)
-end
+After('not @non-zero', &expect_flatware_exit.call(0))
+After('@non-zero', &expect_flatware_exit.call(1))
